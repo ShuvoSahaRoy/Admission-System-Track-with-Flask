c4aa20c0aa579e56b2296f6710d0b26a561882aa
Shuvo Saha Roy <sshuvo548@gmail.com>
2020-08-28 04:33:23 +0600

    email pass added

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -14,7 +14,7 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'ssroy548@gmail.com'
+app.config['MAIL_USERNAME'] = 'ssroy248@gmail.com'
 app.config['MAIL_PASSWORD'] = 'forprojectwork'
 mail= Mail(app)
 
